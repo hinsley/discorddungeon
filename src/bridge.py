5f3a59278d2d11ec915dd4c64687f2c285e91f7f
Carter Hinsley <carterhinsley@outlook.com>
2020-07-04 18:41:19 -0400

    Add entrance to game with DOM reading

--- src/bridge.py
+++ src/bridge.py
@@ -1,16 +1,45 @@
+import time
 from selenium import webdriver
 
 
 options = webdriver.ChromeOptions()
 options.add_argument("headless")
-options.add_argument('window-size=1200x600')
+#options.add_argument('window-size=1200x600')
 
-driver = webdriver.Chrome(chrome_options=options)
+driver = webdriver.Chrome(options=options)
+
+def read_story(driver: webdriver.Chrome) -> str:
+    story = ""
+    poll_frequency = 0.05
+    while True:
+        time.sleep(poll_frequency)
+        prev_len = len(story)
+        try:
+            story = driver.find_element_by_css_selector("span[typinganimationduration]").text
+            if 0 < len(story) == prev_len:
+                return story
+        except:
+            continue
+
+def type_command(driver: webdriver.Chrome, command: str):
+    pass
 
 try:
     driver.get(r"https://play.aidungeon.io/")
 
-    print(driver.find_element_by_css_selector("body").text)
+    while True:
+        try:
+            driver.find_element_by_css_selector("div[aria-label='Play']").click()
+            break
+        except:
+            pass
+    while True:
+        try:
+            driver.find_element_by_css_selector("div[aria-label='New Singleplayer Game']").click()
+            break
+        except:
+            pass
+    print(read_story(driver))
 finally:
     try:
         driver.close()
