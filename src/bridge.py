12c46c584c5fd4c95ef875e3436fd1a29c2f2f9e
Carter Hinsley <carterhinsley@outlook.com>
2020-07-04 19:21:32 -0400

    Get through entire menu system in demo

--- src/bridge.py
+++ src/bridge.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.common.exceptions import StaleElementReferenceException
 
 
 options = webdriver.ChromeOptions()
@@ -8,37 +9,41 @@ options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=options)
 
 def read_story(driver: webdriver.Chrome) -> str:
-    # Wait for the DOM element containing the story to appear.
-    # This is useful when first entering the game, mostly.
     while True:
-        try:        
-            story_element = driver.find_element_by_css_selector("span[typinganimationduration]")
-            break
-        except:
-            pass
-
-    # Determine the frequency at which we should poll the story for cessation of updates.
-    # This is to get around the typewriter animation effect.
-    poll_frequency = float(story_element.get_attribute("typinganimationduration")) / 1000
-
-    # Store the initial story so that we can wait until it actually changes.
-    # This allows us to get around "thinking" time by the AI, as well as DOM update delays
-    # due to rendering or Javascript.
-    story = story_element.text
-    initial_len = len(story)
-
-    while len(story_element.text) == initial_len:
-        pass
-
-    while True:
-        time.sleep(poll_frequency)
-        prev_len = len(story)
         try:
+            # Wait for the DOM element containing the story to appear.
+            # This is useful when first entering the game, mostly.
+            while True:
+                try:        
+                    story_element = driver.find_element_by_css_selector("span[typinganimationduration]")
+                    break
+                except:
+                    pass
+
+            # Determine the frequency at which we should poll the story for cessation of updates.
+            # This is to get around the typewriter animation effect.
+            poll_frequency = float(story_element.get_attribute("typinganimationduration")) / 1000
+
+            # Store the initial story so that we can wait until it actually changes.
+            # This allows us to get around "thinking" time by the AI, as well as DOM update delays
+            # due to rendering or Javascript.
             story = story_element.text
-            # If the story hasn't changed since the last poll, it's done being typed.
-            if len(story) == prev_len:
-                return story
-        except:
+            initial_len = len(story)
+
+            while len(story_element.text) == initial_len:
+                pass
+
+            while True:
+                time.sleep(poll_frequency)
+                prev_len = len(story)
+                try:
+                    story = story_element.text
+                    # If the story hasn't changed since the last poll, it's done being typed.
+                    if len(story) == prev_len:
+                        return story
+                except:
+                    continue
+        except StaleElementReferenceException:
             continue
 
 def send_command(driver: webdriver.Chrome, command: str):
@@ -66,6 +71,11 @@ try:
     print(read_story(driver)) # Read setting selection menu.
     send_command(driver, "1")
     print(read_story(driver)) # Read character selection menu.
+    send_command(driver, "1")
+    print(read_story(driver)) # Read character name prompt.
+    send_command(driver, "John Hancock")
+    print(read_story(driver)) # Read "Generating story...".
+    print(read_story(driver))
 finally:
     try:
         driver.close()
