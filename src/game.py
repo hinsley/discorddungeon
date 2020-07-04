7e3fa1632ff058db67a73e1b0db3bfb43acdf5b5
Carter Hinsley <carterhinsley@outlook.com>
2020-07-04 21:16:15 +0000

    Improve readability a bit

--- src/game.py
+++ src/game.py
@@ -1,26 +1,26 @@
+import enum
 from dataclasses import dataclass
-from enum import Enum
 from typing import List
 
-class Setting(Enum):
-    FANTASY = 1
-    MYSTERY = 2
-    APOCALYPTIC = 3
-    ZOMBIES = 4
-    CYBERPUNK = 5
-    TIGER_KING = 6
-    QUARANTINE = 7
-    CUSTOM = 8
+class Setting(enum.Enum):
+    FANTASY     = enum.auto()
+    MYSTERY     = enum.auto()
+    APOCALYPTIC = enum.auto()
+    ZOMBIES     = enum.auto()
+    CYBERPUNK   = enum.auto()
+    TIGER_KING  = enum.auto()
+    QUARANTINE  = enum.auto()
+    CUSTOM      = enum.auto()
 
 @dataclass
 class StoryChunk():
     """ A piece of history from the established story in a particular game. """
-    player_generated: bool
-    content: str
+    player_generated : bool
+    content          : str
 
 @dataclass
 class Game():
     """ A game's state. """
-    player_name: str
-    history: List[StoryChunk]
-    setting: Setting = Setting.FANTASY
+    player_name : str
+    history     : List[StoryChunk]
+    setting     : Setting = Setting.FANTASY
